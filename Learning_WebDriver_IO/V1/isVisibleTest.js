628a2e7f4ce8f4b32beae79bbc27a238b7bd4ca3
xakc <n3dev@abv.bg>
2021-04-12 21:09:48 +0300

    Adding JS WIDO script for checking if elements are visible

---
+++ Learning_WebDriver_IO/V1/isVisibleTest.js
@@ -0,0 +1,29 @@
+beforeEach(function() {
+  browser.url("/Hidden-Elements/index.html");
+})
+
+describe('Test whether hidden elements are visible', function() {
+  it('Button display is set to non display therefore should return false', function(done) {
+  	var isVisible = browser.isVisible('#not-displayed');
+  	console.log(isVisible);
+  	expect(isVisible).to.equal(false);
+  });
+
+  it('Button display is set to visibility hidden therefore should return false', function(done) {
+  	var isVisible = browser.isVisible('#visibility-hidden');
+  	console.log(isVisible);
+  	expect(isVisible).to.equal(false);
+  });
+
+  it('Button display is set to zero opacity therefore should return false', function(done) {
+  	var isVisible = browser.isVisible('#zero-opacity');
+  	console.log(isVisible);
+  	expect(isVisible).to.equal(false);
+  });
+
+  it('Header text is visible therefore should return true', function(done) {
+  	var isVisible = browser.isVisible('h1');
+  	console.log(isVisible);
+  	expect(isVisible).to.equal(true);
+  });
+});
