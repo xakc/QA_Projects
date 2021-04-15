3109b147cc02538d9d3a493ba406b040fdbe1aa4
xakc <n3dev@abv.bg>
2021-04-15 21:09:44 +0300

    Adding more info

--- Learning_WebDriver_IO/V1/waitForExist_waitForVisible.js
+++ Learning_WebDriver_IO/V1/waitForExist_waitForVisible.js
@@ -10,6 +10,9 @@ describe('Validate whether the click me button is visible', function() {
   });
 
   it('Validate whether the click me button appears once the Ajax loader fully loads', function(done) {
+      this.timeout(20000);
+      var clickMeButton = "//p[text()='CLICK ME!']";
+      browser.waitForVisible(clickMeButton, 8000, false); 
   });
 
 });
