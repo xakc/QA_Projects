32ea9ec7398b2447bbf31f7776095a0c90546724
xakc <n3dev@abv.bg>
2021-04-22 21:33:10 +0300

    Providing edited Sync Data Model by contact page JS code demo

--- Learning_WebDriver_IO/V1/contactUsTest.js
+++ Learning_WebDriver_IO/V1/contactUsTest.js
@@ -13,11 +13,12 @@ describe('Test Contact Us form WebdriverUni', function() {
 		console.log('Inside the describe block!');
 	})
 
+contactusDetails.forEach(function (contactDetail) {
   it('Should be able to submit a successful submission via contact us form', function(done) {
   	browser.setValue("[name='first_name']",'Joe');
   	browser.setValue("[name='last_name']",'Blogs');
-  	browser.setValue("[name='email']", 'joe_blogs@mail.com');
-  	browser.setValue("[name='message']", 'How much does product x cost?');
+  	browser.setValue("[name='email']", contactDetail.email);
+  	browser.setValue("[name='message']", contactDetail.body);
   	browser.click("[type='submit']");
 
   	var successfulContactConfirmation = browser.isExisting('#contact_reply h1');
@@ -25,6 +26,7 @@ describe('Test Contact Us form WebdriverUni', function() {
 
   	var successfulSubmission = browser.getText('#contact_reply h1');
   	expect(successfulSubmission).to.equal('Thank You for your Message!');
+  		})
     });
 
   it('Should not be able to submit a successful submission via contact us form as all fields are required', function(done) {
