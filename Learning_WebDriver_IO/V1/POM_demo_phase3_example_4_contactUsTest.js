9b7422d1ccf33ace2f2a54d0dae9bf74ab3bacac
xakc <n3dev@abv.bg>
2021-05-25 21:21:22 +0300

    POM phase 3 example 4 WIDO JS test

---
+++ Learning_WebDriver_IO/V1/POM_demo_phase3_example_4_contactUsTest.js
@@ -0,0 +1,62 @@
+class ContactUs_Page {
+	get firstName() { return $("[name='first_name']");}
+	get lastName() { return $("[name='last_name']");}
+	get comments() { return $("textarea");}
+	get emailAddress() { return $("[name='email']");}
+	get submitButton() { return $("[type='submit']");}
+	get successfulSubmissionHeader() { return $("#contact_reply h1");}
+	get unsuccessfulSubmissionHeader() { return $("body");}
+}
+
+	setFirstName(firstName) {
+		return this.firstName.setValue(firstName);
+	}
+
+	setLastName(lastName) {
+		return this.lastName.setValue(lastName);
+	}
+
+	setEmailAddress(emailAddress) {
+		return this.emailAddress.setValue(emailAddress);
+	}
+
+	setComments(comments) {
+		return this.comments.setValue(comments);
+	}
+
+	clickSubmitButton() {
+		return this.submitButton.click();
+	}
+
+	submitAllInformationViaContactUsForm(firstName, lastName, emailAddress, comments) {
+		if(firstName) {
+			this.firstName.setValue(firstName);
+		}
+		if(lastName) {
+			this.lastName.setValue(lastName);
+		}
+		if(emailAddress) {
+			this.emailAddress.setValue(emailAddress);
+		}
+		if(comments) {
+			this.comments.setValue(comments);
+		}
+		this.submitButton.click();
+		this.confirmSuccessfulSubmission();
+	}
+
+	confirmSuccessfulSubmission() {
+		var validateSubmissionHeader = browser.waitUntil(function() {
+			return this.successfulSubmissionHeader.getText() == 'Thank You for your Message!'
+		}, 3000)
+		expect(validateSubmissionHeader, 'Successful Submission Message does not Exist!').to.be.true;
+	}
+
+	confirmUnsuccessfulSubmission() {
+		var validateSubmissionHeader = browser.waitUntil(function() {
+			return this.unsuccessfulSubmissionHeader.getText() == 'Error: all fields are required'
+		}, 3000)
+		expect(this.unsuccessfulSubmissionHeader.getText()).to.include('Error: all fields are required');
+	}
+
+module.exports = new ContactUs_Page();
